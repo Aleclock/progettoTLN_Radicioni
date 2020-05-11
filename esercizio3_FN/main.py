97e8e1ba05d06cb20340a23e3e82734c87e3c9c6
Alessandro Clocchiatti <aleclock@MacBook-Pro-di-Alessandro.local>
2020-05-12 01:17:05 +0200

    Create main.py and framseSetStudent.py, added getFrameSetForStudent function

---
+++ esercizio3_FN/main.py
@@ -0,0 +1,11 @@
+from frameSetStudent import getFrameSetForStudent
+
+# cd /Users/aleclock/Desktop/uni/TLN/radicioni/progettoTLN_Radicioni/esercizio3_FN
+     
+
+def main ():
+    dict = getFrameSetForStudent('Clocchiatti')
+    print (len( dict["id"]))
+
+
+main()
